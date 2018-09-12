5a34e70988a74162d2f7a8b0a4b9f6af92af03f1
wy <weiyu@souche.com>
2018-09-12 18:28:07 +0800

    .

---
+++ web/src/main/java/demo/Demo.java
@@ -0,0 +1,12 @@
+package demo;
+
+/**
+ * @author weiyu
+ * @version V1.0
+ * @since 2018-09-12
+ */
+public class Demo {
+    public static void main (String[] args){
+        System.out.println("ddmndm");
+    }
+}
